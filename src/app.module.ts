44cb989be781e464cc8e291d07798fc0ce69d89d
Hervé Eerebout <herve@datacamp.com>
2020-04-14 16:08:08 +0200

    feat: include cron module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,14 @@ import { ConfigModule } from './config/config.module';
 import { MetaController } from './meta/meta.controller';
 import { DashboardModule } from 'dashboard/dashboard.module';
 import { TemperatureModule } from 'temperature/temperature.module';
+import { CronModule } from 'cron/cron.module';
 
 @Module({
   controllers: [MetaController],
   imports: [
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot(databaseConfig),
+    CronModule,
     DashboardModule,
     TemperatureModule,
     ConfigModule,
