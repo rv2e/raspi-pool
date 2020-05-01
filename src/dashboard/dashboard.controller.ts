136e8f267ec1c4030174103be364c5e6a536a450
Hervé Eerebout <herve@datacamp.com>
2020-05-01 20:41:04 +0200

    feat: update dashboard

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,31 +1,56 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { CustomLoggerService } from 'logger/logger.service';
-
+import _ from 'lodash';
 import { OutsideTemperatureService } from 'temperature/outside-temperature.service';
-// import { WaterTemperatureService } from 'temperature/water-temperature.service';
+import { WaterTemperatureService } from 'temperature/water-temperature.service';
+import { BoxTemperatureService } from 'temperature/box-temperature.service';
 
 @Controller('/status')
 export class DashboardController {
   constructor(
-    private readonly outsideTemperatureService: OutsideTemperatureService,
-    // private readonly waterTemperatureService: WaterTemperatureService,
     private readonly customLoggerService: CustomLoggerService,
+    private readonly outsideTemperatureService: OutsideTemperatureService,
+    private readonly waterTemperatureService: WaterTemperatureService,
+    private readonly boxTemperatureService: BoxTemperatureService,
   ) {}
 
   @Get('/')
   @Render('dashboard')
   public async renderDashboard() {
     const outsideTemperatureEntities = await this.outsideTemperatureService.getLastWeekMetrics();
-    // const waterTemperatureEntities = await this.waterTemperatureService.getLastWeekMetrics();
+    const waterTemperatureEntities = await this.waterTemperatureService.getLastWeekMetrics();
+    const boxTemperatureEntities = await this.boxTemperatureService.getLastWeekMetrics();
 
     return {
-      lastTemperature: outsideTemperatureEntities.slice(-1)[0],
-      temperatureData: outsideTemperatureEntities.map(
-        ({ createdAt, temperature }) => ({
-          x: createdAt,
-          y: temperature,
-        }),
-      ),
+      lastBoxTemperature: boxTemperatureEntities.slice(-1)[0],
+      lastWaterTemperature: waterTemperatureEntities.slice(-1)[0],
+      lastOutsideTemperature: outsideTemperatureEntities.slice(-1)[0],
+      plotly: [
+        {
+          type: 'scatter',
+          mode: 'lines',
+          name: 'Outside Temperature',
+          line: { color: 'red' },
+          x: outsideTemperatureEntities.map(({ createdAt }) => createdAt),
+          y: outsideTemperatureEntities.map(({ temperature }) => temperature),
+        },
+        {
+          type: 'scatter',
+          mode: 'lines',
+          name: 'Box Temperature',
+          line: { color: 'orange' },
+          x: boxTemperatureEntities.map(({ createdAt }) => createdAt),
+          y: boxTemperatureEntities.map(({ temperature }) => temperature),
+        },
+        {
+          type: 'scatter',
+          mode: 'lines',
+          name: 'Water Temperature',
+          line: { color: 'black' },
+          x: waterTemperatureEntities.map(({ createdAt }) => createdAt),
+          y: waterTemperatureEntities.map(({ temperature }) => temperature),
+        },
+      ],
     };
   }
 }
--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -3,11 +3,18 @@ import { CustomLoggerModule } from 'logger/logger.module';
 
 import { DashboardController } from 'dashboard/dashboard.controller';
 import { TemperatureModule } from 'temperature/temperature.module';
+import { LightModule } from 'light/light.module';
+import { VerticalActuatorModule } from 'vertical-actuator/vertical-actuator.module';
 
 @Module({
   controllers: [DashboardController],
   exports: [],
-  imports: [CustomLoggerModule, TemperatureModule],
+  imports: [
+    CustomLoggerModule,
+    TemperatureModule,
+    LightModule,
+    VerticalActuatorModule,
+  ],
   providers: [],
 })
 export class DashboardModule {}
